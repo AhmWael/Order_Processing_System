20a48f939c007a3a7421619b9feb6f447159e622
Hamza Walid <hamzawalidcr@gmail.com>
2025-12-24 14:47:26 +0200

    feat(frontend-auth): update landing and signup UI

--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -1,54 +1,140 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 export default function SignupPage() {
   const fieldClassName =
     "bg-indigo-300 ring-indigo-600 ring-1 text-indigo-950 text-center rounded-xl";
 
+  const buttonClassName =
+    "text-violet-400 rounded-xl bg-indigo-800 hover:bg-indigo-900 transition px-5 py-4 text-center";
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [error, setError] = useState("");
+  const response = fetch("local");
+  function handleSubmit() {}
   return (
-    <div>
-      <div className="min-h-screen flex items-center justify-center bg-linear-to-tr from-blue-200 to-indigo-500">
-        <div className="bg-linear-to-br from-gray-700 to-black rounded-xl shadow-2xl p-8 ring-2 ring-indigo-700">
-          <h1 className="text-center font-bold text-indigo-500 text-3xl">
-            Sign Up
-          </h1>
-          <div className="flex flex-col gap-5">
-            <div className="flex flex-row gap-4">
-              <div>
-                <p className="text-left text-indigo-800">First name</p>
-                <input className={fieldClassName}></input>
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-tr from-gray-900">
+      <Card className="bg-black w-full max-w-md ">
+        <CardHeader>
+          <CardTitle className="text-gray-500">Create an account</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="Username" className="text-gray-500">
+                  Username
+                </Label>
+                <Input
+                  id="Username"
+                  className="text-gray-400"
+                  type="text"
+                  required
+                />
               </div>
-              <div>
-                <p className="text-left text-indigo-800">Last name</p>
-                <input className={fieldClassName}></input>
+              <div className="flex flex-row gap-8">
+                <div className="grid gap-2">
+                  <Label htmlFor="Firstname" className="text-gray-500">
+                    First Name
+                  </Label>
+                  <Input
+                    id="Firstname"
+                    className="text-gray-400"
+                    type="text"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="Lastname" className="text-gray-500">
+                    Last Name
+                  </Label>
+                  <Input
+                    id="Lastname"
+                    className="text-gray-400"
+                    type="text"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="flex flex-row gap-8">
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label className="text-gray-500" htmlFor="password">
+                      Password
+                    </Label>
+                  </div>
+                  <Input
+                    className="text-gray-400"
+                    id="password"
+                    type="password"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label className="text-gray-500" htmlFor="re-password">
+                      Re-enter Password
+                    </Label>
+                  </div>
+                  <Input
+                    className="text-gray-400"
+                    id="re-password"
+                    type="password"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="Email" className="text-gray-500">
+                  Email
+                </Label>
+                <Input
+                  className="text-gray-400"
+                  placeholder="x@example.com"
+                  id="Email"
+                  type="Email"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="Address" className="text-gray-500">
+                  Address
+                </Label>
+                <Input
+                  className="text-gray-400"
+                  id="Address"
+                  type="text"
+                  required
+                />
               </div>
             </div>
-            <div>
-              <p className="text-left text-indigo-800">Username</p>
-              <input className={fieldClassName}></input>
-            </div>
-            <div className="flex flex-row gap-4">
-              <div>
-                <p className="text-left text-indigo-800">Password</p>
-                <input type="password" className={fieldClassName}></input>
-              </div>
-              <div>
-                <p className="text-left text-indigo-800">re-enter Password</p>
-                <input type="password" className={fieldClassName}></input>
-              </div>
-            </div>
-            <div>
-              <p className="text-left text-indigo-800">Email address</p>
-              <input className={fieldClassName}></input>
-            </div>
-            <div>
-              <p className="text-left text-indigo-800">Phone number</p>
-              <input className={fieldClassName}></input>
-            </div>
-            <div>
-              <p className="text-left text-indigo-800">Shipping address</p>
-              <input className={fieldClassName}></input>
-            </div>
-          </div>
-        </div>
-      </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Register
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,25 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Alert } from "@/components/ui/alert";
 
 export default function HomePage() {
   const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     // Check if user is logged in
@@ -22,31 +37,78 @@ export default function HomePage() {
         }
       }
     };
-
     checkAuth();
   }, [router]);
 
-  const buttonClassName =
-    "text-violet-400 rounded-xl bg-indigo-800 hover:bg-indigo-900 transition px-5 py-4 text-center";
+  function handleSubmit() {
+    if (!username || !password) {
+      alert("Please enter username and password");
+      return;
+    }
+  }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-linear-to-tr from-blue-200 to-indigo-500">
-      <div className="bg-linear-to-br from-gray-700 to-black rounded-xl shadow-2xl p-8 ring-2 ring-indigo-700">
-        <h1 className="text-center font-bold text-indigo-500 text-3xl">
-          Welcome to our Bookstore!
-        </h1>
-        <p className="text-center text-indigo-400 mb-7">
-          Sign-in or create an account to continue
-        </p>
-        <div className="flex gap-3 justify-center">
-          <Link href="/login" className={buttonClassName}>
-            login
-          </Link>
-          <Link href="/signup" className={buttonClassName}>
-            signup
-          </Link>
-        </div>
-      </div>
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-tr from-gray-900">
+      <Card className="bg-black w-full max-w-sm ">
+        <CardHeader>
+          <CardTitle className="text-gray-500">Login to your account</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="Username" className="text-gray-500">
+                Username
+              </Label>
+              <Input
+                id="Username"
+                className="text-gray-400"
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label className="text-gray-500" htmlFor="password">
+                  Password
+                </Label>
+                <a
+                  href="#"
+                  className="text-gray-500 ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+
+              <Input
+                className="text-gray-400"
+                id="password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </CardContent>
+
+        <CardFooter className="flex-col gap-2">
+          <Button
+            onClick={handleSubmit}
+            type="submit"
+            className="cursor-pointer w-full"
+          >
+            Login
+          </Button>
+          <a
+            href="/signup"
+            className="text-gray-500 text-left text-sm underline-offset-4 hover:underline"
+          >
+            Sign Up
+          </a>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
